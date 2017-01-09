a1768f9c4aecf1d7f965a3f2956a60804e50ce68
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-01-09 15:08:51 -0500

    added reactive event map (frontend only)

--- client/main.js
+++ client/main.js
@@ -1,3 +1,11 @@
 import './routes/defaultRoutes.js'
 import './routes/eventRoutes.js'
 import './routes/userControlRoutes.js'
+
+if (Meteor.isClient) {
+	Meteor.startup(function() {
+		GoogleMaps.load({
+			key: 'AIzaSyAox36VrlsVU2wRfAJ8R94ysdsFts9o1EU' 
+		});
+	});
+}
--- imports/ui/helpers/eventHelpers.js
+++ imports/ui/helpers/eventHelpers.js
@@ -2,6 +2,24 @@ import '../templates/events.html';
 import '../../api/mapHandlers/initMap.js'
 
 Template.mixrEventMap.onCreated(function(){
-	console.log("on main map")
-	sayHello()
+	GoogleMaps.ready('mixrMap', function(map) {
+		var latLng = Geolocation.latLng();
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(latLng.lat, latLng.lng),
+			map: map.instance
+		});
+	});
+});
+
+Template.mixrEventMap.helpers({
+	initMapOptions: function() {
+		var latLng = Geolocation.latLng();
+		// Initialize the map once we have the latLng.
+		if (GoogleMaps.loaded() && latLng) {
+			return {
+				center: new google.maps.LatLng(latLng.lat, latLng.lng),
+				zoom: 8
+			};
+		}
+	}
 });
