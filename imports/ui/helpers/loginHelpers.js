39b005413fdcbe392d44693184f67104b8402e17
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-01-08 23:38:32 -0500

    adding notification modals, reorg routes

--- imports/ui/helpers/loginHelpers.js
+++ imports/ui/helpers/loginHelpers.js
@@ -1,23 +1,21 @@
 import '../templates/loginPage.html';
-
+import '../templates/modals.html';
 Template.loginPage.events({
 	'submit .loginForm'(event, template) {
 		event.preventDefault()
 		var loginEmail = event.target.emailLogin.value;
 		var loginPassword = event.target.passwordLogin.value;
-		var loginAttempt = attemptLoginClient(loginEmail, loginPassword)
-		if (loginAttempt){
-			console.log("logged in")
-		}
+		attemptLoginClient(loginEmail, loginPassword)
 	}
 });
 
 function attemptLoginClient(email, pwd){
 	Meteor.loginWithPassword(email, pwd, function(err){
 		if (err) {
-			console.log("Invalid username or password.")
-			return 0
+			Modal.show('loginFailedNotification')
+		}
+		else{
+			Router.go('home')
 		}
 	});
-	return 1
 }
