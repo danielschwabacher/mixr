2459ecc74d6af2a813068bc1b5de21501d765f66
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-01-11 21:10:05 -0500

    got reactive settings page working

--- imports/ui/helpers/settingsHelpers.js
+++ imports/ui/helpers/settingsHelpers.js
@@ -1,12 +1,36 @@
 import '../templates/settingsPage.html'
 import './changePasswordModalHelpers.js'
-
-Template.settingsPage.onCreated(function(){
-
+Template.settingsPage.onCreated(function () {
+	Session.set('default', true)
 });
-Template.settingsPage.events({
+
+Template.leftSettingsPane.events({
 	'click .changePasswordLink'(event, template){
 		Modal.show('changePasswordModal')
+	},
+	'click .mapSettingsLink'(event, template){
+		setMapSettings()
+	},
+	'click .manageEventsLink'(event, template){
+		setManageEvents()
 	}
-
 });
+
+Template.activeContent.helpers({
+  	getCurrentFocus: function(){
+ 		return Session.get('default') ? 'mapSettingsPanel' : 'manageEventsPanel'
+  	}
+});
+
+Template.mapSettingsPanel.onCreated(function(){
+});
+
+Template.manageEventsPanel.onCreated(function(){
+});
+
+setManageEvents = function(){
+	Session.set('default', false)
+}
+setMapSettings = function(){
+	Session.set('default', true)
+}
