41e824a6ebeeda53ebdf07f93593693325c6521c
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-01-12 22:42:56 -0500

    events show up in respective places on manageEvents page

--- imports/ui/helpers/settingsHelpers.js
+++ imports/ui/helpers/settingsHelpers.js
@@ -26,29 +26,55 @@ Template.mapSettingsPanel.onCreated(function(){
 });
 
 Template.manageEventsPanel.onCreated(function(){
-	Meteor.subscribe("userEventsCrossReference", function(){
-		client_users_events = UserEventsCrossReferenceCollection.findOne(
-			{
-				user: Meteor.userId(),
-			},
-			{
-				fields: { '_id': 0,'owned_events.eventId': 1 }
-			}
-		)
-		Session.set('ownedEvents', client_users_events)
-		//console.log("first owned event: " + JSON.stringify(client_users_events['owned_events'][0]['eventId']))
-	});
+	//console.log("first owned event: " + JSON.stringify(client_users_events['owned_events'][0]['eventId']))
 });
-
 Template.manageEventsPanel.helpers({
-	getOwnedEvents: function(){
-		return JSON.stringify(Session.get('ownedEvents'))
+	'getOwnedEventIds': function(){
+		users_events = UserEventsCrossReferenceCollection.findOne({user: Meteor.userId()},{fields: {'owned_events.eventId': 1}})
+		if (users_events){
+			ownedEventsId = []
+			for (i = 0; i < users_events['owned_events'].length; i++){
+				ownedEventsId.push({eventId: users_events['owned_events'][i].eventId})
+			}
+			return ownedEventsId
+		}
+		else{
+			return 0
+		}
+    },
+	'getRegisteredEventIds': function(){
+		users_events = UserEventsCrossReferenceCollection.findOne({user: Meteor.userId()},{fields: {'registered_events.eventId': 1}})
+		if (users_events){
+			registeredEventIds = []
+			for (i = 0; i < users_events['registered_events'].length; i++){
+				registeredEventIds.push({eventId: users_events['registered_events'][i].eventId})
+			}
+			return registeredEventIds
+		}
+		else{
+			return 0
+		}
 	},
-	events: [
-		{ text: 'This is event 1' },
-		{ text: 'This is event 2' },
-		{ text: 'This is event 3' },
-	]
+	'getAssociatedEventNames': function(eventIdsObj){
+		eventIds = []
+		eventNames = []
+		if (eventIdsObj){
+			for (i = 0; i < eventIdsObj.length; i++){
+				eventIds.push(eventIdsObj[i].eventId)
+			}
+			eventNamesObj = EventCollection.find(
+				{ _id: { $in: eventIds} },
+				{ fields: {event_name: 1} }
+			)
+			eventNamesObj.forEach(function(item){
+				eventNames.push({eventName: item.event_name})
+			});
+			return eventNames
+		}
+		else{
+			return [{eventName: "You have no events"}]
+		}
+	}
 });
 
 
