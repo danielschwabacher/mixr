3c3adfe815fed7f6ddfdda05d45f63c0e25e0921
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-01-24 19:36:02 -0700

    modals generated by by the change password routines now autofocus

--- imports/ui/helpers/changePasswordModalHelpers.js
+++ imports/ui/helpers/changePasswordModalHelpers.js
@@ -2,25 +2,23 @@ import '../templates/changePasswordModal.html';
 import '../templates/notificationModals.html';
 Template.changePasswordModal.events({
 	'submit .changePasswordModalForm'(event, template) {
-		console.log("clicked changePassword")
 		event.preventDefault()
-		console.log("target: " + JSON.stringify(event.target))
 		var oldPassword = event.target.currentPasswordChangePasswordModal.value;
 		var newPassword1 = event.target.newPassword1.value;
 		var newPassword2 = event.target.newPassword2.value;
 		if (newPassword1 == newPassword2){
 			Accounts.changePassword(oldPassword, newPassword1, function(err){
 				if (err){
-					alert("password could not be changed.")
+					Modal.show("passwordChangeFailedModal");
 				}
 				else{
 					Modal.hide(template)
-					Modal.show('passwordChangedModal')
+					Modal.show("passwordChangedModal");
 				}
 			});
 		}
 		else{
-			Modal.show('passwordsDoNotMatchModal')
+			Modal.show('passwordsDoNotMatchModal');
 		}
 	}
 });
