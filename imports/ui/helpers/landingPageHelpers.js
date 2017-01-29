918c0ff75f17c7c654643a54ec84007f54518e7a
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-01-28 22:56:10 -0700

    template landing page done

--- imports/ui/helpers/landingPageHelpers.js
+++ imports/ui/helpers/landingPageHelpers.js
@@ -1,1 +1,9 @@
 import '../templates/landingPage.html';
+Template.landingPage.events({
+	'click .scroll-down-button'(event) {
+		event.preventDefault();
+		$(document.body).animate({
+    		'scrollTop': $('#whatIsMixr').offset().top
+		}, 1000);
+	}
+});
