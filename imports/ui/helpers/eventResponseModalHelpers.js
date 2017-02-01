c69e06a8ff3df37f66da237dc859f95b15535585
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-01-31 23:33:22 -0700

    tag modals displaying properly

--- imports/ui/helpers/eventResponseModalHelpers.js
+++ imports/ui/helpers/eventResponseModalHelpers.js
@@ -3,14 +3,15 @@ import '../templates/eventResponseModals.html';
 import '../templates/applyEventFilterModals.html';
 
 Template.sortByTagsEventFilterModal.onCreated(function() {
-	console.log("created event tag modal")
 });
 
 Template.sortByTagsEventFilterModal.events({
 	'click #submitEventFilterTagSelections'(event, template){
 		event.preventDefault()
-		var eventSelectedTag = $("input[type='checkbox']:checked");
-		var literalEventTag = eventSelectedTag.attr('id');
-		console.log("checked is: " + literalEventTag)
+		var checkedValues = $('input:checkbox:checked').map(function() {
+    		return this.id;
+		}).get();
+		console.log("checked is: " + checkedValues)
+		console.log("reload map with filters applied")
 	}
 });
