eb0c70230bca1c1bbd90f5a9a7c6b528c58a4b41
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-02-25 15:02:50 -0700

    working on settings page refactor

--- imports/ui/helpers/manageEventsHelpers.js
+++ imports/ui/helpers/manageEventsHelpers.js
@@ -48,3 +48,81 @@ Template.manageEventsPanel.helpers({
 		}
 	}
 });
+
+Template.eventRegisteredNameHolder.events({
+	'click .nameLinkRegistered'(event, template){
+		context = this
+		eventInfo = EventCollection.findOne({_id: context.eventId})
+		if (eventInfo){
+			Modal.show('dynamicModalRegistered', eventInfo)
+		}
+	}
+});
+
+Template.eventCreatedNameHolder.events({
+	'click .nameLinkCreated'(event, template){
+		context = this
+		eventInfo = EventCollection.findOne({_id: context.eventId})
+		if (eventInfo){
+			Modal.show('dynamicModalCreated', eventInfo)
+		}
+	}
+});
+
+Template.dynamicModalRegistered.events({
+	'click .unregisterEventRegisteredModal'(event, template){
+		var self = this
+		Meteor.call('unregisterEvent', self._id)
+	}
+});
+
+Template.dynamicModalCreated.events({
+	'click .deleteEventCreatedModal'(event, template){
+		var self = this
+		Meteor.call('deleteEvent', self._id)
+	}
+});
+
+
+
+Template.dynamicModalRegistered.helpers({
+	returnContextualEventName: function(){
+			return this.event_name
+	},
+	returnContextualEventLocation: function(){
+		return this.event_location
+	},
+	returnContextualEventLocation: function(){
+		return this.event_location
+	},
+	returnContextualEventDescription: function(){
+		return this.event_description
+	},
+	returnContextualEventDateTime: function(){
+		return this.event_dateTime
+	},
+	returnContextualNumberAttending: function(){
+		return this.number_of_users_attending
+	}
+});
+
+Template.dynamicModalCreated.helpers({
+	returnContextualEventName: function(){
+			return this.event_name
+	},
+	returnContextualEventLocation: function(){
+		return this.event_location
+	},
+	returnContextualEventLocation: function(){
+		return this.event_location
+	},
+	returnContextualEventDescription: function(){
+		return this.event_description
+	},
+	returnContextualEventDateTime: function(){
+		return this.event_dateTime
+	},
+	returnContextualNumberAttending: function(){
+		return this.number_of_users_attending
+	}
+})
