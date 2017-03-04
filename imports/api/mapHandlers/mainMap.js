faf746c87c7f9847f5e351ec931c1d1a0a6690fe
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-03-03 20:22:55 -0700

    infoBubbles now working correctly

--- imports/api/mapHandlers/mainMap.js
+++ imports/api/mapHandlers/mainMap.js
@@ -5,10 +5,9 @@ import '../InfoBubble/infobubble-compiled.js'
 Marker = function(mapInstance, content){
 	this.map = mapInstance
 	this.eventInfo = content
+	this.bubble = null
 }
-Marker.prototype.getName = function(){
-	return this.eventInfo.event_name;
-}
+
 Marker.prototype.createObjectMarker = function(){
 	// console.log("creating marker: " + JSON.stringify(this.eventInfo))
 	eventPosition = {lat: this.eventInfo.event_position.latitude, lng: this.eventInfo.event_position.longitude}
@@ -16,27 +15,37 @@ Marker.prototype.createObjectMarker = function(){
 	eventDescription = this.eventInfo.event_description
 	eventTag = this.eventInfo.event_tag
 
-
 	var eventMarker = new google.maps.Marker({
 		position: eventPosition,
 		map: this.map
 	});
 
-	markerInfo = '<div id="iw-container">' + '<div class="iw-content">' + '<div class="iw-subTitle">Event name: </div>' + eventName +'<div class="iw-subTitle">Event description: </div>' + eventDescription +'<div class="iw-subTitle">Tag: </div>' + eventTag + '</div>' + '</div>'
+	var markerInfo = '<div id="iw-container">' + '<div class="iw-content">' + '<div class="iw-subTitle">Event name: </div>' + eventName + '<div class="iw-subTitle">Tag: </div>' + eventTag + '</div>' + '</div>'
 
 	eventDisplayBubble = new InfoBubble({
 		content: markerInfo,
 		backgroundColor: 'black',
 		borderRadius: 4,
 		arrowSize: 10,
-		maxWidth: 300,
 		disableAutoPan: true
 	});
 
-	google.maps.event.addListener(eventMarker, 'click', function() {
-		console.log("clicked marker: " + this.eventInfo.event_name)
-		eventDisplayBubble.setContent(markerInfo);
-		eventDisplayBubble.open(this.map, eventMarker);
+	this.bubble = eventDisplayBubble
+	/*
+		the arrow functions are because we need to refer to the 'this' object representing the marker (outside of the nested function).
+	*/
+	google.maps.event.addListener(eventMarker, 'click', () => {
+		Modal.show('eventInformationModal', this.eventInfo)
+	});
+
+	google.maps.event.addListener(eventMarker, 'mouseover', () => {
+		// console.log("clicked marker: " + this.eventInfo.event_name)
+		this.bubble.setContent(markerInfo);
+		this.bubble.open(this.map, eventMarker);
+	});
+
+	google.maps.event.addListener(eventMarker, 'mouseout', () => {
+		this.bubble.close()
 	});
 
 	GLOBAL_MARKERS.push(eventMarker)
--- imports/ui/helpers/mixrPrimaryMapHelpers.js
+++ imports/ui/helpers/mixrPrimaryMapHelpers.js
@@ -30,9 +30,7 @@ Template.mixrEventMap.onRendered(function(){
 			);
 			client_collection.forEach(function(currentEvent){
 				temp_marker = new Marker(map.instance, currentEvent)
-				// console.log("temp_marker is: " + temp_marker)
 				temp_marker.createObjectMarker()
-				// temp_marker = null
 			});
         });
 	});
