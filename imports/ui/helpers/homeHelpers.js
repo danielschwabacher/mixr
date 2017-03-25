e2205943d75468122cdf09b5ceebd7d21f95a8f0
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-03-25 00:05:12 -0400

    Working on making website responsive. Changed navbars to collapsable navbars sitewide.

--- imports/ui/helpers/homeHelpers.js
+++ imports/ui/helpers/homeHelpers.js
@@ -1,6 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import '../templates/home.html';
+// import '../templates/home.html';
 
 document.title = "mixr™"
