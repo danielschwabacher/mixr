a69d10147cd55c834b579028e43ae836201e5a39
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-08-25 20:18:25 -0600

    have to fix the notifications onSumbit of forgot password modal.

--- imports/ui/helpers/forgotPasswordModalHelpers.js
+++ imports/ui/helpers/forgotPasswordModalHelpers.js
@@ -1,16 +1,18 @@
 import '../templates/forgotPasswordModal.html';
 import '../templates/changePasswordModal.html';
+import '../../api/Notifications/notifyWrapper.js';
 
 Template.forgotPasswordModal.events({
-  'submit .forgotPasswordModalForm'(event, template) {
-    event.preventDefault();
-    console.log('Fired from submit event');
-    var userEmail = event.target.userEmailForgotPasswordModal.value;
-    Accounts.forgotPassword(userEmail, function(error) {
-      if (error) {
-        console.log(error.reason);
-      } else {
-        console.log("Email successfully sent!");
-      }
-    });
+    'submit .forgotPasswordModalForm'(event, template) {
+        event.preventDefault();
+        var userEmail = event.target.userEmailForgotPasswordModal.value;
+        Accounts.forgotPassword(userEmail, function(error) {
+          if (error) {
+              notify("Email could not be sent.", "danger", "center")
+          } 
+          else {
+            notify("Email reset link sent!", "success", "right")
+          }
+        });
+    }
 });
